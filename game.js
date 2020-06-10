4aa95d944bc52bc60173fc614014099612dbb0a7
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-09 17:23:32 -0700

    created variable that randomly picks one of the 4 game's colors

--- game.js
+++ game.js
@@ -1,10 +1,15 @@
 buttonColours = ["red", "blue", "green","yellow"]
 
-
 nextSequence();
 
 function nextSequence (){
     // random # between 0-3
     var randomNumber = Math.floor(Math.random()*4) ;
     console.log(randomNumber);
+    
+    // uses button color array $ random number
+    // to generate a color picked at random
+    var randomChosenColour = buttonColours[randomNumber];
+    console.log(randomChosenColour);
+
 }
