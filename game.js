f031438e082aa2248908a9ec361b43ab801139e6
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-09 17:21:26 -0700

    random number generates 0-3

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-// js linked
+buttonColours = ["red", "blue", "green","yellow"]
+
+
+nextSequence();
+
+function nextSequence (){
+    // random # between 0-3
+    var randomNumber = Math.floor(Math.random()*4) ;
+    console.log(randomNumber);
+}
