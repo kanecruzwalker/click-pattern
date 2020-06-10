a8e410efb16b13ccd6dd611f3fe76efcaae70a1d
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-09 18:18:12 -0700

    added startOver function for restarts, though it currently restarts too quickly and straight to level 1 not start page.

--- game.js
+++ game.js
@@ -51,6 +51,8 @@ function checkAnswer(currentLevel) {
         }, 200);
 
         $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
     }
 }
 
@@ -95,7 +97,9 @@ function animatePress(currentColor) {
     }, 100);
 }
 
-
-// game patterns not being pushed or made
-console.log(gamePattern);
-console.log(userClickedPattern);
+// function to restart after lost
+function startOver(){
+    level= 0;
+    gamePattern = [];
+    started = false;
+}
